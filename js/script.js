cde0a56c5d26edfaa9984098dc8ee79c56d62588
Simon Piirainen <simon.piirainen@gmail.com>
2014-12-04 21:06:27 +0100

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 $(function(){
 
+	var flag = 1;
+	$("button").click(function() {
+		
+            if (flag == 1) {
+                $('#result').stop(true).animate({
+                	'margin-bottom': 0,
+                	'opacity': '1' },
+                	{ queue: false,
+            		  duration: 300 });
+                flag = 0;
+            }
+            else {
+                $('#result').stop(true).animate({
+                	'margin-bottom': -70,
+                	'opacity': '0' },
+                	{ queue: false,
+        			  duration: 300 });
+                flag = 1;
+            }
+
+            return false;
+	})
+
 
 /* ----------------------- GlOBAL VARIABLES ----------------------- */
 	var latitude = 0;
